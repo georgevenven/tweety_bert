6f37e9bac2c0ecb33bb383ac11b3c594736ded69
George Vengrovski <georgevenven@gmail.com>
2024-08-21 15:53:37 -0700

    commands for ROSE computer

--- figure_generation_scripts/dim_reduced_birdsong_plots.py
+++ figure_generation_scripts/dim_reduced_birdsong_plots.py
@@ -13,9 +13,57 @@ sys.path.append("src")
 from utils import load_model
 from analysis import plot_umap_projection
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-## This should be CPU so that we are never limited by GPU memory ## <-- change this if your system memory is less than VRAM or you have a ton of VRAM
-device = torch.device("cpu")
+weights_path = "experiments/Sham_Lesion_5271/saved_weights/model_step_23500.pth"
+config_path = "experiments/Sham_Lesion_5271/config.json"
+
+model = load_model(config_path, weights_path)
+model = model.to(device)
+
+plot_umap_projection(
+    model=model, 
+    device=device, 
+    data_dirs=["/media/george-vengrovski/Extreme SSD/sham lesioned birds/USA5271_group_1",
+                "/media/george-vengrovski/Extreme SSD/sham lesioned birds/USA5271_group_2",
+                "/media/george-vengrovski/Extreme SSD/sham lesioned birds/USA5271_group_3",
+                "/media/george-vengrovski/Extreme SSD/sham lesioned birds/USA5271_group_4"],
+    samples=1e6, 
+    category_colors_file="/home/george-vengrovski/Downloads/category_colors_llb3.pkl", 
+    layer_index=-2, 
+    dict_key="attention_output", 
+    context=1000, 
+    raw_spectogram=False,
+    save_dict_for_analysis=True,
+    save_name="SHAM_USA5271",
+    plot_comparison=True,
+    truncate_to_smallest_group= False
+)
+
+weights_path = "experiments/Sham_Lesion_5283/saved_weights/model_step_36500.pth"
+config_path = "experiments/Sham_Lesion_5283/config.json"
+
+model = load_model(config_path, weights_path)
+model = model.to(device)
+
+plot_umap_projection(
+    model=model, 
+    device=device, 
+    data_dirs=["/media/george-vengrovski/Extreme SSD/sham lesioned birds/USA5283_group_1",
+                "/media/george-vengrovski/Extreme SSD/sham lesioned birds/USA5283_group_2",
+                "/media/george-vengrovski/Extreme SSD/sham lesioned birds/USA5283_group_3",
+                "/media/george-vengrovski/Extreme SSD/sham lesioned birds/USA5283_group_4"],
+    samples=1e6, 
+    category_colors_file="/home/george-vengrovski/Downloads/category_colors_llb3.pkl", 
+    layer_index=-2, 
+    dict_key="attention_output", 
+    context=1000, 
+    raw_spectogram=False,
+    save_dict_for_analysis=True,
+    save_name="SHAM_USA5283",
+    plot_comparison=True,
+    truncate_to_smallest_group= False
+)
 
 weights_path = "experiments/LLB3_Whisperseg/saved_weights/model_step_27000.pth"
 config_path = "experiments/LLB3_Whisperseg/config.json"
@@ -26,60 +74,15 @@ model = model.to(device)
 plot_umap_projection(
     model=model, 
     device=device, 
-    data_dirs=["/media/george-vengrovski/Extreme SSD/sham lesioned birds/USA5271_group_1", "/media/george-vengrovski/Extreme SSD/sham lesioned birds/USA5271_group_2", "/media/george-vengrovski/Extreme SSD/sham lesioned birds/USA5271_group_3", "/media/george-vengrovski/Extreme SSD/sham lesioned birds/USA5271_group_4"],
-    samples=1e4, 
-    category_colors_file="/home/george-vengrovski/Documents/projects/tweety_bert_paper/files/category_colors_llb3.pkl", 
+    data_dirs=["/media/george-vengrovski/Extreme SSD/yarden_data/llb3_test", "/media/george-vengrovski/Extreme SSD/yarden_data/llb3_train"],
+    samples=4e6, 
+    category_colors_file="/home/george-vengrovski/Downloads/category_colors_llb3.pkl", 
     layer_index=-2, 
     dict_key="attention_output", 
     context=1000, 
     raw_spectogram=False,
     save_dict_for_analysis=True,
-    save_name="TEST_SYNTAX",
+    save_name="Yarden_LLB3_Whisperseg",
     plot_comparison=True,
     truncate_to_smallest_group= False
 )
-
-
-# weights_path = "experiments/Sham_Lesion_5271/saved_weights/model_step_23500.pth"
-# config_path = "experiments/Sham_Lesion_5271/config.json"
-
-# model = load_model(config_path, weights_path)
-# model = model.to(device)
-
-# plot_umap_projection(
-#     model=model, 
-#     device=device, 
-#     data_dirs=["/media/george-vengrovski/Extreme SSD/sham lesioned birds/USA5271_before_3-07", "/media/george-vengrovski/Extreme SSD/sham lesioned birds/USA5271_after_3-07"],
-#     samples=1e6, 
-#     category_colors_file="/home/george-vengrovski/Downloads/category_colors_llb3.pkl", 
-#     layer_index=-2, 
-#     dict_key="attention_output", 
-#     context=1000, 
-#     raw_spectogram=False,
-#     save_dict_for_analysis=True,
-#     save_name="SHAM_USA5271",
-#     plot_comparison=True,
-#     truncate_to_smallest_group= False
-# )
-
-# weights_path = "experiments/Sham_Lesion_5283/saved_weights/model_step_36500.pth"
-# config_path = "experiments/Sham_Lesion_5283/config.json"
-
-# model = load_model(config_path, weights_path)
-# model = model.to(device)
-
-# plot_umap_projection(
-#     model=model, 
-#     device=device, 
-#     data_dirs=["/media/george-vengrovski/Extreme SSD/sham lesioned birds/USA5283_before_3-05", "/media/george-vengrovski/Extreme SSD/sham lesioned birds/USA5283_after_3-05"],
-#     samples=1e6, 
-#     category_colors_file="/home/george-vengrovski/Downloads/category_colors_llb3.pkl", 
-#     layer_index=-2, 
-#     dict_key="attention_output", 
-#     context=1000, 
-#     raw_spectogram=False,
-#     save_dict_for_analysis=True,
-#     save_name="SHAM_USA5283",
-#     plot_comparison=True,
-#     truncate_to_smallest_group= False
-# )
