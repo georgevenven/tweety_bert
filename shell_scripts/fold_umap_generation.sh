4e4a82313e821e1748e55e79c10d5f717b4150d5
George <georgevenven@gmail.com>
2024-12-07 15:35:29 -0800

    fixed issue with specs not being generated

--- shell_scripts/fold_umap_generation.sh
+++ shell_scripts/fold_umap_generation.sh
@@ -31,28 +31,30 @@ python scripts/copy_files_from_wavdir_to_multiple_event_dirs.py \
     --songs_per_fold 100
 
 # Process each fold
-for fold_dir in "$TEMP_DIR/folds"/fold_*; do
+echo "Looking for folds in: $TEMP_DIR/folds"
+ls -la "$TEMP_DIR/folds" || echo "No folds directory or empty!"
+
+for fold_dir in "$TEMP_DIR/folds"/group_*; do
+    echo "Processing: $fold_dir"
     if [ -d "$fold_dir" ]; then
-        fold_number=$(basename "$fold_dir" | grep -oP 'fold_\K\d+')
+        fold_number=$(basename "$fold_dir" | grep -oP 'group_\K\d+')
         spec_dir="$SPECS/fold_$fold_number"
         mkdir -p "$spec_dir"
-        
+
         echo "Generating spectrograms for fold $fold_number..."
-        echo "Source directory: $fold_dir"
-        echo "Destination directory: $spec_dir"
         python src/spectogram_generator.py \
             --src_dir "$fold_dir" \
             --dst_dir "$spec_dir" \
             --song_detection_json_path "$SONG_DETECTION_JSON_PATH" \
-            || echo "Error generating spectrograms"
-        
+            >"$spec_dir/log.txt" 2>&1
+
         save_name="${BIRD_NAME}_fold${fold_number}"
         echo "Running UMAP for Fold: $fold_number"
         python figure_generation_scripts/dim_reduced_birdsong_plots.py \
             --experiment_folder "experiments/$MODEL_NAME" \
             --data_dir "$spec_dir" \
             --save_name "$save_name" \
-            --samples 1000
+            --samples 1e6
     fi
 done
 
